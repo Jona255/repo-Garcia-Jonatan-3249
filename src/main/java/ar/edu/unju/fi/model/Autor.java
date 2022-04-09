327c19f64651277c188cd25d98927dd5034cc6ab
Jonatan Garcia <sacha.255555@gmail.com>
2022-04-09 20:08:25 -0300

    corregidos errores al hacer merge

--- src/main/java/ar/edu/unju/fi/model/Autor.java
+++ src/main/java/ar/edu/unju/fi/model/Autor.java
@@ -1,9 +1,5 @@
 package ar.edu.unju.fi.model;
 
-<<<<<<<< HEAD:src/main/java/ar/edu/unju/fi/model/Autor.java
 public class Autor {
-========
-public class Socio {
->>>>>>>> req2-socio:src/main/java/ar/edu/unju/fi/model/Socio.java
 
 }
